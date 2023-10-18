dba4f5d2eecf5e00612d3ca3d4d535f60b0be28b
kotbarsik96 <kotbarsik96@gmail.com>
2023-10-18 12:34:32 +0400

    спойлеры

---
+++ frontend/src/components/misc/SpoilerElem.vue
@@ -0,0 +1,97 @@
+<template>
+    <component :is="tag" class="spoiler">
+        <button class="spoiler__button" type="button" @click="toggle">
+            <slot name="buttonContent"></slot>
+        </button>
+        <Transition :css="false" @before-enter="onBeforeEnter" @enter="onEnter" @leave="onLeave">
+            <component v-show="isBodyShown" :is="spoilerBodyTag" class="spoiler__body" :class="spoilerBodyClass"
+                ref="spoilerBody">
+                <slot></slot>
+            </component>
+        </Transition>
+    </component>
+</template>
+
+<script>
+import { gsap } from 'gsap'
+import { getHeight } from '@/assets/js/scripts.js'
+
+function tagValidator(value) {
+    const availableTags = [
+        'div',
+        'ul',
+        'li'
+    ]
+    return availableTags.includes(value)
+}
+export default {
+    name: 'SpoilerElem',
+    props: {
+        isShownDefault: Boolean,
+        tag: {
+            type: String,
+            default: 'div',
+            validator: tagValidator
+        },
+        spoilerBodyTag: {
+            type: String,
+            default: 'div',
+            validator: tagValidator
+        },
+        spoilerBodyClass: String
+    },
+    data() {
+        return {
+            isBodyShown: false
+        }
+    },
+    mounted() {
+        this.isShownDefault
+            ? this.show()
+            : this.hide()
+    },
+    methods: {
+        toggle() {
+            this.isBodyShown
+                ? this.hide()
+                : this.show()
+        },
+        async show() {
+            this.isBodyShown = true
+        },
+        async hide() {
+            this.isBodyShown = false
+        },
+        onBeforeEnter(el){
+            gsap.set(el, {
+                maxHeight: '0px',
+                padding: '0px',
+                margin: '0px'
+            })
+        },
+        onEnter(el, done){
+            const maxHeight = `${getHeight(el)}px`
+            gsap.to(el, {
+                maxHeight,
+                duration: .3,
+                onComplete: done
+            })
+        },
+        onLeave(el, done){
+            gsap.to(el, {
+                maxHeight: '0px',
+                padding: '0px',
+                margin: '0px',
+                duration: .3,
+                onComplete: done
+            })
+        },
+    }
+}
+</script>
+
+<style>
+.spoiler__body {
+    overflow: hidden;
+}
+</style>
